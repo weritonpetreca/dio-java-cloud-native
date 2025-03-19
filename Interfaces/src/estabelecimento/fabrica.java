39e6ff82ec57a9fce3dfe3cb98520a1d57c4362b
Weriton Luis Petreca <eulcf@hotmail.com>
2025-03-19 19:31:52 -0300

    feat atividades aulas

---
+++ Interfaces/src/estabelecimento/fabrica.java
@@ -0,0 +1,19 @@
+package estabelecimento;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+import equipamentos.multifuncional.EquipamentoMultifuncional;
+
+public class fabrica {
+    public static void main(String[] args) {
+        EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+        Impressora impressora = em;
+        Digitalizadora digitalizadora = em;
+        Copiadora copiadora = em;
+        
+        impressora.imprimir();
+        digitalizadora.digitalizar();
+        copiadora.copiar();
+    }
+}
