d2f274fb8ba2d7851a4ce0271dbefdf18720b7cd
Weriton Luis Petreca <eulcf@hotmail.com>
2025-03-25 11:19:47 -0300

    fixed IMC ex

--- exercicios-java-basico/estruturas-de-controle/IMC/src/Calculo.java
+++ exercicios-java-basico/estruturas-de-controle/IMC/src/Calculo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Calculo {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
--- exercicios-java-basico/estruturas-de-controle/IMC/src/IMC.java
+++ exercicios-java-basico/estruturas-de-controle/IMC/src/IMC.java
@@ -3,20 +3,19 @@ import java.text.DecimalFormat;
 public class IMC {
     public IMC (double weight, double heigh){
         double IMC = weight / (heigh * heigh);
-        DecimalFormat formato = new DecimalFormat("#,##");
-        IMC = Double.valueOf(formato.format(IMC));
+        String result = String.format("%.2f", IMC);
         if (IMC <= 18.5){
-            System.out.println("Seu IMC é " + IMC+ ", você se enquadra na categoria de Abaixo do peso.");
+            System.out.println("Seu IMC é " + result+ ", você se enquadra na categoria de Abaixo do peso.");
         } else if (IMC <= 24.9){
-            System.out.println("Seu IMC é " + IMC + ", você se enquadra na categoria de Peso ideal.");
+            System.out.println("Seu IMC é " + result + ", você se enquadra na categoria de Peso ideal.");
         } else if (IMC <= 29.9){
-            System.out.println("Seu IMC é " + IMC + ", você se enquadra na categoria de Levemente acima do peso.");
+            System.out.println("Seu IMC é " + result + ", você se enquadra na categoria de Levemente acima do peso.");
         } else if (IMC <= 34.9){
-            System.out.println("Seu IMC é " + IMC + ", você se enquadra na categoria de Obesidade Grau I.");
+            System.out.println("Seu IMC é " + result + ", você se enquadra na categoria de Obesidade Grau I.");
         } else if (IMC <= 39.9){
-            System.out.println("Seu IMC é " + IMC + ", você se enquadra na categoria de Obesidade Grau II (Severa).");
+            System.out.println("Seu IMC é " + result + ", você se enquadra na categoria de Obesidade Grau II (Severa).");
         } else {
-            System.out.println("Seu IMC é " + IMC + ", você se enquadra na categoria de Obesidade Grau III (Mórbida).");
+            System.out.println("Seu IMC é " + result + ", você se enquadra na categoria de Obesidade Grau III (Mórbida).");
         }
 
     }
