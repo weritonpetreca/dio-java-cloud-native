adc6fc305cb4b5e1b01b1ace6dd9774f30efbeb2
Weriton Luis Petreca <eulcf@hotmail.com>
2025-04-29 14:28:12 -0300

    feat started RESTful API challenge

---
+++ dio-challenge/src/main/java/com/petreca/dio_challenge/Application.java
@@ -0,0 +1,13 @@
+package com.petreca.dio_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
