81699fef6076a6c94f52a4a3a38f04b1a035d03a
Weriton Luis Petreca <eulcf@hotmail.com>
2025-04-29 15:40:33 -0300

    feat config string

---
+++ dio-challenge/src/main/java/com/petreca/dio_challenge/model/User.java
@@ -0,0 +1,76 @@
+package com.petreca.dio_challenge.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Cascade;
+
+import java.util.List;
+
+
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Feature> feature;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<News> news;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeature() {
+        return feature;
+    }
+
+    public void setFeature(List<Feature> feature) {
+        this.feature = feature;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+}
---
+++ dio-challenge/src/main/java/com/petreca/dio_challenge/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.petreca.dio_challenge.repository;
+
+import com.petreca.dio_challenge.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
